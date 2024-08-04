d7503dfd3d393ee43be36964c8e38ba2e9ee33f2
WithersChat <witherschat@gmail.com>
2024-08-04 06:46:53 +0200

    ULV covers just dropped!

--- kubejs/server_scripts/Early_Game._Components.js
+++ kubejs/server_scripts/Early_Game._Components.js
@@ -82,6 +82,85 @@ ServerEvents.recipes(event => {
 
        }
     )
+    //ULV components cheaper
+    event.shaped(
+        'ulvcovm:ulv_electric_motor',
+        [
+            'TR',
+            'MT'
+        ],
+        {
+            T: 'gtceu:tin_single_wire',
+            R: 'gtceu:copper_rod',
+            M: 'gtceu:magnetic_iron_rod'
+        }
+    ).id('ulvcovm:shaped/ulv_electric_motor')
 
+    event.shaped(
+        'ulvcovm:ulv_electric_piston',
+        [
+            'PR',
+            'MG'
+        ],
+        {
+            P: 'gtceu:copper_plate',
+            R: 'gtceu:copper_rod',
+            M: 'ulvcovm:ulv_electric_motor',
+            G: 'gtceu:small_copper_gear'
+        }
+    ).id('ulvcovm:shaped/ulv_electric_piston')
+
+    event.shaped(
+        'ulvcovm:ulv_robot_arm',
+        [
+            'MR',
+            'PC'
+        ],
+        {
+            P: 'ulvcovm:ulv_electric_piston',
+            R: 'gtceu:copper_rod',
+            M: 'ulvcovm:ulv_electric_motor',
+            C: '#gtceu:circuits/ulv'
+        }
+    ).id('ulvcovm:shaped/ulv_robot_arm')
+
+    event.shaped(
+        'ulvcovm:ulv_electric_pump',
+        [
+            'PR',
+            'WM'
+        ],
+        {
+            P: 'gtceu:copper_normal_fluid_pipe',
+            R: 'gtceu:copper_rotor',
+            M: 'ulvcovm:ulv_electric_motor',
+            W: 'gtceu:tin_single_wire'
+        }
+    ).id('ulvcovm:shaped/ulv_electric_pump')
+
+    event.shaped(
+        'ulvcovm:ulv_fluid_regulator',
+        [
+            'TR',
+            'MT'
+        ],
+        {
+            T: '#gtceu:circuits/ulv',
+            R: 'gtceu:tin_single_wire',
+            M: 'ulvcovm:ulv_electric_pump'
+        }
+    ).id('ulvcovm:shaped/ulv_fluid_regulator')
+
+    event.shaped(
+        'ulvcovm:ulv_conveyor_module',
+        [
+            'RR',
+            'MM'
+        ],
+        {
+            R: 'gtceu:sticky_resin',
+            M: 'ulvcovm:ulv_electric_pump'
+        }
+    ).id('ulvcovm:shaped/ulv_electric_conveyor')
 
 })
