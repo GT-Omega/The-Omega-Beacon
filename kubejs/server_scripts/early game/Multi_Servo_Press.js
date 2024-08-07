c944f732618ad677933bfc7c49f4fab1aebda2ba
Spicierspace153 <cameronjmiller@outlook.com>
2024-08-07 11:50:16 -0600

    Create Multi_Servo_Press.js
    erm excuqueese me sauce,
    are those gtceu plate recipes that i see for the multiservo press

---
+++ kubejs/server_scripts/early game/Multi_Servo_Press.js
@@ -0,0 +1,30 @@
+ServerEvents.recipes(event => {
+
+//GTCEUM Plates     event.recipes.thermal.press('2x gtceu:tin_plate', '3x gtceu:tin_ingot'
+event.recipes.thermal.press('2x gtceu:copper_plate', '3x minecraft:copper_ingot')
+event.recipes.thermal.press('2x gtceu:tin_plate', '3x gtceu:tin_ingot')
+event.recipes.thermal.press('2x gtceu:lead_plate', '3x gtceu:lead_ingot')  
+event.recipes.thermal.press('2x gtceu:steel_plate', '3x gtceu:steel_ingot')
+event.recipes.thermal.press('2x gtceu:iron_plate', '3x gtceu:tin_ingot')
+event.recipes.thermal.press('2x gtceu:copper_plate', '3x minecraft:ingot_ingot')
+event.recipes.thermal.press('2x gtceu:red_alloy_plate', '3x gtceu:red_alloy_ingot')
+
+
+//firebrick 
+event.recipes.thermal.press('gtceu:compressed_fireclay', 
+    ['gtceu:fireclay_dust', 'gtceu:ingot_casting_mold'])
+
+
+})
+
+
+//add thermal tag to all gtmolds :Tr:
+ServerEvents.tags('item', event =>{
+
+    event.add('thermal:crafting/dies','gtceu:ingot_casting_mold')
+    event.add('thermal:crafting/dies','gtceu:gear_casting_mold')
+    
+    
+})
+    
+    
