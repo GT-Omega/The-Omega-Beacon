3853455fbbee5737e6452d479e3a24dd93c481c9
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2024-08-09 17:00:33 +0400

    Remove KJS clay plate

---
+++ kubejs/startup_scripts/Item_Registry.js
@@ -0,0 +1,4 @@
+StartupEvents.registry('item', => {
+  event.create('unfired_ball_cast').texture('kubejs:item/unfired_ball_cast')
+  event.create('fired_ball_cast').texture('kubejs:item/fired_ball_cast')
+})
--- kubejs/startup_scripts/item_regestry.js
+++
@@ -1,4 +0,0 @@
-
-event.create('unfired_ball_cast').texture('kubejs:item/unfired_ball_cast')
-event.create('fired_ball_cast').texture('kubejs:item/fired_ball_cast')
-event.create('clay_plate')
